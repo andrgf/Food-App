bc3676d4fd2c68f6feba0c3a97c573a822a02160
andrgf <gfandre3@gmail.com>
2023-08-14 06:49:10 +0700

    change handler postdelayed

--- app/src/main/java/com/example/indonesiafood/MainActivity.kt
+++ app/src/main/java/com/example/indonesiafood/MainActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
+import android.os.Looper
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,7 +13,7 @@ class MainActivity : AppCompatActivity() {
 
         supportActionBar?.hide()
 
-        Handler().postDelayed({
+        Handler(Looper.getMainLooper()).postDelayed({
             val intent = Intent(this@MainActivity, HomeIndonesianFood::class.java)
             startActivity(intent)
             finish()
